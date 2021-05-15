02c090199addd334a8b8f84c02bc3b39fbb0fa0d
amitaiger <82775079+amitaiger@users.noreply.github.com>
2021-05-15 07:05:37 +0300

    Add files via upload

--- Benchmarks/arraysloops.c
+++ Benchmarks/arraysloops.c
@@ -1,74 +1,80 @@
 void assert(int cond);
-#define SWAP(X,Y) { tmp = (X); (X) = (Y); (Y) = tmp; }
 
 //gets max of array in index 0 <= i <= n
 int max_array (int a[], int n){
 	assert(n >= 0);
-	int i, max, var1, var2;
+	int i, max, var1;
 	max = a[0];
 	for (i = 0; i < n; i++){
-		assert(!(0 <= var1 <= i && !(max >= a[var1])));
+		assert(!((0 <= var1 && var1 < i) && !(max >= a[var1])));
 		if (a[i] > max){
 			max = a[i];
 		}
 	}
 	return max;
-	assert(!(0 <= var2 <= n && !(max >= a[var2])));
+	assert(!((0 <= var1 && var1 < n) && !(max >= a[var1])));
 }
 
-//sorts array in range 0 <= i < n
-int bubble_sort (int a[], int n){
+//gets max of array in index 0 <= i <= n, but with wrong invariant
+int max_array_wrong (int a[], int n){
 	assert(n >= 0);
-	int i, j, tmp, var1, var2, var3, var4, var5, var6;
-	for (i = 0; i < n-1; i++){
-		assert (!(n-i <= var1 < n && !(!(0 <= var2 < n-i && !(a[var1] >= a[var2])))) &&
-				!((n-i <= var3 < n && n-i <= var4 < n && var3 <= var4) && !(a[var3] <= a[var4])));
-		for (j = 0; j < n-i-1; j++){
-			assert (!(0 <= var3 < j && !(a[j] >= a[var3])));
-			if (a[j] > a[j+1]) SWAP (a[j], a[j+1])
+	int i, max, var1;
+	max = a[0];
+	for (i = 0; i < n; i++){
+		assert(!((0 <= var1 && var1 < n) && !(max >= a[var1])));
+		if (a[i] > max){
+			max = a[i];
 		}
 	}
-	assert (!(((0 <= var5 < n) && (0 <= var6 < n) && (var5 <= var6)) && !(a[var5] <= a[var6])));
+	return max;
+	assert(!((0 <= var1 && var1 <= n) && !(max >= a[var1])));
 }
 
-//sorts array in range 0 <= i < n, but with an intentionally wrong loop invariant
-int bubble_sort_wrong (int a[], int n){
+//checks if array is sorted
+int is_sorted (int a[], int n){
+	assert (n > 0);
+	int i, var1;
+	for (i = 0; i < n - 1; i++){
+		assert(!((0 <= var1 && var1 < i) && !(a[var1] <= a[var1+1])));
+		if (a[i] > a[i+1]){
+			return 0;
+			assert(a[i] == a[i+1]);
+		}
+	}
+	return 1;
+	assert(!((0 <= var1 && var1 < n - 1) && !(a[var1] <= a[var1+1])));
+}
+
+//checks if array is sorted, but implementation is wrong
+int is_sorted_wrong (int a[], int n){
+	assert (n > 0);
+	int var1;
+	if (a[i] > a[i+1]){
+		return 0;
+		assert(a[i] == a[i+1]);
+	}
+	return 1;
+	assert(!((0 <= var1 && var1 < n) && !(a[var1] <= a[var1+1])));
+}
+
+//squares every entry in array and puts in array square
+void square_array (int a[], int square[], int n){
 	assert(n >= 0);
-	int i, j, tmp, var1, var2, var3, var4, var5, var6;
-	for (i = 0; i < n-1; i++){
-		assert (!(n-i <= var1 < n && !(!(0 <= var2 < n-i && !(a[var1] >= a[var2])))) &&
-				!((n-i <= var3 < n && n-i <= var4 < n && var3 <= var4) && !(a[var3] <= a[var4])));
-		for (j = 0; j < n-i-1; j++){
-			assert (!(0 <= var3 < i && !(a[j] >= a[var3])));
-			if (a[j] > a[j+1]) SWAP (a[j], a[j+1])
-		}
+	int i, var1;
+	for (i = 0; i < n; i++){
+		assert(!((0 <= var1 && var1 < i) && !(square[var1] == a[var1]*a[var1])));
+		square[i] = a[i]*a[i];
 	}
-	assert (!(((0 <= var5 < n) && (0 <= var6 < n) && (var5 <= var6)) && !(a[var5] <= a[var6])));
+	assert(!((0 <= var1 && var1 < n) && !(square[var1] == a[var1]*a[var1])));
 }
 
-//takes a sorted array and finds i so that a[i] == x. If no such i exists, returns -1
-int binary_search (int a[], int n, int x){
-	assert (!(n >= 0 && (((0 <= var1 < n) && (0 <= var2 < n) && (var1 <= var2)) && !(a[var1] <= a[var2]))));
-	int start, end, mid, var1, var2, var3;
-	start = 0;
-	end = n - 1;
-	mid = (start+end)/2;
-	while (start <= end){
-		assert (!(a[mid] == x && !(start <= mid <= end)));
-		if (a[mid] == x){
-			return mid;
-			assert (a[mid] == x);
-		}
-		else {
-			if (a[mid] < x){
-				start = mid+1;
-			}
-			else { //a[mid] > x
-				end = mid-1;
-			}
-		}
-		mid = (start+end)/2;
+//squares every entry in array and puts in array square, but with wrong invariant
+void square_array_wrong (int a[], int square[], int n){
+	assert(n >= 0);
+	int i, var1;
+	for (i = 0; i < n; i++){
+		assert(square[i] == a[i]*a[i]);
+		square[i] = a[i]*a[i];
 	}
-	return -1;
-	assert (!(0 <= var3 < n && !(a[var3] != x)));
+	assert(!((0 <= var1 && var1 < n) && !(square[var1] == a[var1]*a[var1])));
 }
